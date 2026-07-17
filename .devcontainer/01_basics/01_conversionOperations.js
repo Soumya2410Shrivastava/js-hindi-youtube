bb949f30c40967174f564b447c5eccd852279c9c
Soumya2410Shrivastava <2410soumyashrivastava@gmail.com>
2026-07-17 09:59:41 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_conversionOperations.js
@@ -0,0 +1,10 @@
+let score=44;
+console.log(typeof (score));
+let name="44"
+console.log(typeof(name));
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+let string=33;
+let stringNumber=String(string)
+console.log(stringNumber);
